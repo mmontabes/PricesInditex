0cf458558afea9eeb325e99907d5c24656afb2ff
Miguel <miguel.montabes@gmail.com>
2025-03-12 19:02:39 +0100

    Global exceptions and service added

---
+++ src/main/java/com/inditex/price/application/PriceService.java
@@ -0,0 +1,37 @@
+package com.inditex.price.application;
+
+
+import com.inditex.price.domain.Price;
+import com.inditex.price.domain.port.PriceQuery;
+import com.inditex.price.exceptions.PriceNotFoundException;
+import com.inditex.price.infrastructure.adapter.persistence.PriceEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class PriceService implements PriceQuery {
+
+    private final PriceRepository priceRepository;
+
+    @Autowired
+    public PriceService(PriceRepository priceRepository) {
+        this.priceRepository = priceRepository;
+    }
+
+    @Override
+    public Price getPrice(LocalDateTime date, Long productId, Long brandId) {
+            List<PriceEntity> prices = priceRepository.findByProductIdAndBrandIdAndStartDateBeforeAndEndDateAfterOrderByPriorityDesc(
+                            productId, brandId, date, date);
+            if (prices.isEmpty()) {
+                throw new PriceNotFoundException("No price found for the given parameters");
+            }
+            PriceEntity priceEntity = prices.get(0);
+            return new Price(priceEntity.getProductId(), priceEntity.getBrandId(),
+                    priceEntity.getPriceList(), priceEntity.getStartDate(),
+                    priceEntity.getEndDate(), priceEntity.getPrice(), priceEntity.getCurr());
+        }
+    }
+
